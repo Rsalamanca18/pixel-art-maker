611fed9136c77b61fece5c21284d41b7bd196ba1
Rodrigo Salamanca <salamanca_rod18@yahoo.com>
2018-08-09 20:24:19 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('.square');
+  square.addEventListener('onclick', function(event){
+    event.style.background = red;
+  })
+})
